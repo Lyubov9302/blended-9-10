de70a547ba77eb37cb824b8f1b4050ff2fd8e43b
Lyubov9302 <lyubovyurynets@gmail.com>
2025-07-15 20:43:09 +0300

    +++

---
+++ src/js/handler.js
@@ -0,0 +1,10 @@
+
+// логіка при сабміті
+
+const tasks = [];
+
+export function onHeaderFormSubmit(evt) {
+evt.preventDefault();
+console.log('Hello');
+refs;
+}
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+// посилання на дом-елементи
+
+export const refs = {
+    headerForm: document.querySelector('.header-form'),
+
+};
